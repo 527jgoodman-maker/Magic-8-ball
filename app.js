5480352d4c97b454c748d17bd0b5791d65db59a7
Justin-Goodman <527jgoodman@frhsd.com>
2025-12-09 14:26:34 +0000

    Finished commit

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ function shakeMagic8Ball () {
 
   switch (randomNumber) {
     case 0:
-      answer = 'Yes'
+      answer = 'Absolutely, for sure!'
       color = 'text-success'
       break
     case 1:
-      answer = 'No, but nice try!',
+      answer = 'Not in this lifetime.'
       color = 'text-danger'
       break
     case 2:
@@ -35,23 +35,23 @@ function shakeMagic8Ball () {
       color = 'text-warning'
       break
     case 3:
-      answer = 'Ask again later'
-      color = 'text-secondary'
+      answer = 'That is uncertain...'
+      color = 'text-warning'
       break
     case 4:
       answer = 'Cannot predict now, Im buffering.',
       color = 'text-muted'
       break
     case 5:
-      answer = "Don't count on it"
+      answer = 'Highly doubtful.'
       color = 'text-danger'
       break
     case 6:
-      answer = 'You may rely on it, but double-check.',
-      color = 'text-success'
+      answer = 'Chances are slim.'
+      color = 'text-danger'
       break
     case 7:
-      answer = 'Outlook not so good, but I’m just a ball.',
+      answer = 'Outlook not good, but Im just a ball.',
       color = 'text-danger'
       break
     default:
